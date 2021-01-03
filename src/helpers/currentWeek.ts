d3d42164989e9318d3fb95d3aabc150f56eebd35
Brandon Myers <brandonstrasn91@yahoo.com>
2021-01-03 11:45:28 -0500

    fix current week for last week

--- src/helpers/currentWeek.ts
+++ src/helpers/currentWeek.ts
@@ -1,6 +1,7 @@
-import getWeek from 'date-fns/getWeek';
-import sub from 'date-fns/sub';
+// import getWeek from 'date-fns/getWeek';
+// import sub from 'date-fns/sub';
 export default () => {
-  const w = getWeek(sub(new Date(), { days: 2 })) - 36;
-  return w > 0 ? w : (w + 36) % 17 === 0 ? 1 : (w + 36) % 17;
+  return 17;
+  // const w = getWeek(sub(new Date(), { days: 2 })) - 36;
+  // return w > 0 ? w : (w + 36) % 17 === 0 ? 1 : (w + 36) % 17;
 };
