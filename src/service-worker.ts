1ac5e206d31a4736404bb812b47619b38d3502ec
Brandon Myers <brandonstrasn91@yahoo.com>
2021-10-29 10:59:53 -0400

    update service worker messaging

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -97,18 +97,20 @@ firebase.initializeApp({
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(payload => {
-  console.log(
-    '[firebase-messaging-sw.js] Received background message ',
-    payload
-  );
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
+if (firebase.messaging.isSupported()) {
+  const messaging = firebase.messaging();
+  messaging.setBackgroundMessageHandler(payload => {
+    console.log(
+      '[firebase-messaging-sw.js] Received background message ',
+      payload
+    );
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+      body: 'Background Message body.',
+      icon: '/firebase-logo.png'
+    };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+    self.registration.showNotification(notificationTitle, notificationOptions);
+  });
+}
